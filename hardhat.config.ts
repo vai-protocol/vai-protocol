acfcd062a8b4d4deafabb156e966587aa40a4a16
Ethan Ray <rayethan335@gmail.com>
2025-05-30 16:17:38 +0700

    enable optimizer

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -6,8 +6,19 @@ dotenv.config();
 const { BSC_RPC_URL, BSC_TESTNET_RPC_URL, PRIVATE_KEY } = process.env;
 
 const config: HardhatUserConfig = {
-  solidity: "0.8.28",
+  solidity: {
+    version: "0.8.28",
+    settings: {
+      optimizer: {
+        enabled: true,
+        runs: 800,
+      },
+    },
+  },
   networks: {
+    hardhat: {
+      allowUnlimitedContractSize: false,
+    },
     bsc: {
       url: BSC_RPC_URL || "",
       chainId: 56,
