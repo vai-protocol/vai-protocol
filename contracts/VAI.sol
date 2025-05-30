ab32ed430f213d67db58a84ddc6acb8ec8b69fbf
Ethan Ray <rayethan335@gmail.com>
2025-05-30 17:00:04 +0700

    create VAI token

---
+++ contracts/VAI.sol
@@ -0,0 +1,22 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+contract VAI is ERC20, Ownable {
+    constructor(
+        string memory name,
+        string memory symbol,
+        uint256 initialSupply,
+        address initialOwner
+    ) ERC20(name, symbol) Ownable(initialOwner) {
+        if (initialSupply > 0) {
+            _mint(initialOwner, initialSupply);
+        }
+    }
+
+    function mint(address to, uint256 amount) public onlyOwner {
+        _mint(to, amount);
+    }
+}
